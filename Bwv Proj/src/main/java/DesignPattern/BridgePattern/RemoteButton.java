db6cdb5edf54eafa1f66f7abd2441f6c05ac0f01
Zcapok <Stan.Kopacz@googlemail.com>
2023-08-15 11:00:16 +0200

    Bridge Pattern

---
+++ Bwv Proj/src/main/java/DesignPattern/BridgePattern/RemoteButton.java
@@ -0,0 +1,25 @@
+package DesignPattern.BridgePattern;
+
+public abstract class RemoteButton {
+    
+    private EntertaimentDevice theDevice;
+
+    public RemoteButton(EntertaimentDevice newDevice){
+        theDevice = newDevice;
+    }
+
+    public void buttonFivePressed(){
+        theDevice.buttonFivePressed();
+    }
+
+    public void buttonSixPressed(){
+        theDevice.buttonSixPressed();
+    }
+
+    public void deviceFeedback(){
+        theDevice.deviceFeedback();
+    }
+
+    public abstract void buttonNinePressed();
+
+}
---
+++ Bwv Proj/src/main/java/DesignPattern/BridgePattern/TVRemoteMute.java
@@ -0,0 +1,15 @@
+package DesignPattern.BridgePattern;
+
+public class TVRemoteMute extends RemoteButton {
+
+    public TVRemoteMute(EntertaimentDevice newDevice) {
+        super(newDevice);
+       
+    }
+
+    @Override
+    public void buttonNinePressed() {
+        System.out.println("TV was Muted");
+    }
+    
+}
