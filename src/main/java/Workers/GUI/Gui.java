ad88938defa643f557f0d3986a37adfde4128d5c
Zcapok <Stan.kopacz@googlemail.com>
2023-09-22 14:37:56 +0200

    dfs

--- src/main/java/Workers/GUI/Gui.java
+++ src/main/java/Workers/GUI/Gui.java
@@ -38,7 +38,7 @@ public class Gui extends JFrame {
         // Spalten füllen mit Werten
         for (Mitarbeiter m : list) {
             // Erzeugen Sie ein Array von Objekten für jede Zeile
-            Object[] row = { m.getID(), m.getName(), m.einkommen() };
+            Object[] row = {m.getID(), m.getName(), m.einkommen()};
             // Fügen Sie die Zeile dem TableModel hinzu
             tableModel.addRow(row);
         }
@@ -48,42 +48,43 @@ public class Gui extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JTextField idField = new JTextField();
-                JTextField nameField = new JTextField();
-                JTextField gehaltField = new JTextField();
+        JTextField nameField = new JTextField();
+        JTextField gehaltField = new JTextField();
 
-                Object[] fields = {
-                        "ID:", idField,
-                        "Name:", nameField,
-                        "Gehalt:", gehaltField
-                };
+        Object[] fields = {
+            "ID:", idField,
+            "Name:", nameField,
+            "Gehalt:", gehaltField
+        };
 
-                int result = JOptionPane.showConfirmDialog(
-                        frame,
-                        fields,
-                        "Mitarbeiter hinzufügen",
-                        JOptionPane.OK_CANCEL_OPTION);
+        int result = JOptionPane.showConfirmDialog(
+            frame,
+            fields,
+            "Mitarbeiter hinzufügen",
+            JOptionPane.OK_CANCEL_OPTION
+        );
 
-                if (result == JOptionPane.OK_OPTION) {
-                    try {
-                        // Die eingegebenen Daten aus den Textfeldern abrufen
-                        int id = Integer.parseInt(idField.getText());
-                        String name = nameField.getText();
-                        double gehalt = Double.parseDouble(gehaltField.getText());
+        if (result == JOptionPane.OK_OPTION) {
+            try {
+                // Die eingegebenen Daten aus den Textfeldern abrufen
+                int id = Integer.parseInt(idField.getText());
+                String name = nameField.getText();
+                double gehalt = Double.parseDouble(gehaltField.getText());
 
-                        // Einen neuen Mitarbeiter erstellen und zur Liste hinzufügen
-                        Mitarbeiter neuerMitarbeiter = new BueroArbeiter(id, name, gehalt);
-                        list.add(neuerMitarbeiter);
+                // Einen neuen Mitarbeiter erstellen und zur Liste hinzufügen
+                Mitarbeiter neuerMitarbeiter = new BueroArbeiter(id, name, gehalt);
+                list.add(neuerMitarbeiter);
 
-                        // Die neue Zeile zur Tabelle hinzufügen
-                        Object[] row = { neuerMitarbeiter.getID(), neuerMitarbeiter.getName(),
-                                neuerMitarbeiter.einkommen() };
-                        tableModel.addRow(row);
-                    } catch (NumberFormatException ex) {
-                        JOptionPane.showMessageDialog(frame, "Ungültige Eingabe. ID und Gehalt müssen Zahlen sein.",
-                                "Fehler", JOptionPane.ERROR_MESSAGE);
-                    }
-                }
+                // Die neue Zeile zur Tabelle hinzufügen
+                Object[] row = {neuerMitarbeiter.getID(), neuerMitarbeiter.getName(), neuerMitarbeiter.einkommen()};
+                tableModel.addRow(row);
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(frame, "Ungültige Eingabe. ID und Gehalt müssen Zahlen sein.", "Fehler", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    
 
+                
             }
         });
 
@@ -93,11 +94,10 @@ public class Gui extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 int selectedRow = table.getSelectedRow();
                 if (selectedRow >= 0) {
-                    // Entfernen des ausgewählten Mitarbeiters
+                    // Entfernen des ausgewählten Mitarbeiters 
                     tableModel.removeRow(selectedRow);
                 } else {
-                    JOptionPane.showMessageDialog(frame, "Bitte eine Zeile auswählen.", "Fehler",
-                            JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(frame, "Bitte eine Zeile auswählen.", "Fehler", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
