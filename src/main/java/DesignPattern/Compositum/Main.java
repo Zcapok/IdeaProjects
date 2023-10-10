eee89bd55210704b3bf28f8744f052c38b923b96
Zcapok <Stan.Kopacz@googlemail.com>
2023-10-10 15:00:29 +0200

    bugfixes Compositum Main

--- src/main/java/DesignPattern/Compositum/Main.java
+++ src/main/java/DesignPattern/Compositum/Main.java
@@ -1,12 +1,18 @@
 package DesignPattern.Compositum;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
+
 
 public class Main {
     
     public static void main(String[] args){
-        //ArrayList<Figur2D> figurliste = new ArrayList<>();
+
+        Box box1 = new VideoGame("1", 100);
+        Box box2 = new Book("Hamlet", 10);
+
+        CompositeBox listprice = new CompositeBox(box1,box2);
+        System.out.println(listprice.calculatePrice());
+
 
         Figur2D kreis = new Kreis(2);
         Figur2D rechteck = new Rechteck(4,7);
@@ -20,19 +26,6 @@ public class Main {
         for(Figur2D s : Kompositumliste.getTeileliste()){
             System.out.println(s.flaeche());
         }
-    }
-
-=======
-public class Main {
-    
-    public static void main(String[] args){
-
-        Box box1 = new VideoGame("1", 100);
-        Box box2 = new Book("Hamlet", 10);
-
-        CompositeBox listprice = new CompositeBox(box1,box2);
-        System.out.println(listprice.calculatePrice());
 
     }
->>>>>>> 06d928863d004784304f1e26bc25e2865468d880
 }
