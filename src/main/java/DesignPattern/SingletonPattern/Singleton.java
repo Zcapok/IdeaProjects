ce054320aa412b240893052a63fd45480c10c080
Zcapok <Stan.kopacz@googlemail.com>
2023-10-16 19:59:09 +0200

    Singleton

--- src/main/java/DesignPattern/SingletonPattern/Singleton.java
+++ src/main/java/DesignPattern/SingletonPattern/Singleton.java
@@ -4,12 +4,7 @@ public final class Singleton {
 
     private static Singleton instanz = new Singleton();
 
-<<<<<<< HEAD
-    private Singleton(){   
-    }
-=======
     private Singleton(){}
->>>>>>> 7f64acdb231d5d7758a957e40f5bffb4275d87e1
 
     public static Singleton getInstanz(){
         
